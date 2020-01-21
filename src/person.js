9b41950e827c6eca102b978f2e6ce1c26442d5c9
kurtlin <kurtlin.hendricks@umuzi.org>
2020-01-21 11:02:48 +0200

    second commit

--- src/person.js
+++ src/person.js
@@ -1,24 +1,34 @@
 class Person {
+  constructor(name, age, gender, interests) {
+    this.age = age;
+    this.name = name;
+    this.gender = gender;
+    this.interests = interests;
+  }
 
-    constructor(name, age, gender, interests) {
-        this.age = age
-        this.name = name
-        this.gender = gender
-        this.interests = interests
-    }
-
-    hello(){
-          return "hello my name is, " + this.name + "and i am " + this.age + "years old. " +this.interests 
-    }
+  hello() {
+    return (
+      " hello, my name is " +
+      this.name +
+      " and i am " +
+      this.age +
+      " years old. My interest are " +
+      this.interests[0] +
+      ", " +
+      this.interests[1] +
+      " and " +
+      this.interests[2]
+    );
+  }
 }
- 
 
 let person = new Person("Ryan", 30, "male", [
-                        "being a hardarse",
-                        "agile",
-                        "ssd hard drives"])
+  "being a hardarse",
+  "agile",
+  "ssd hard drives"
+]);
 
-let greeting = person.hello()
- 
-module.exports = 
-    Person
+let greeting = person.hello();
+console.log(greeting);
+
+module.exports = Person;
